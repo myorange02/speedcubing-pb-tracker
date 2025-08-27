7e4447956485133eede8fb8baac45cbbc07f4ab1
Juwhan Jung <myorange02@Juwhanui-MacBookPro.local>
2025-08-27 20:53:33 +0900

    chore: scaffold repo and Firebase web init

---
+++ frontend/src/firebase.js
@@ -0,0 +1,5 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDl6SWSYTMRawWPGcc1uju3YIyoFUvYTe4",
+    authDomain: "speedcubing-pb-tracker.firebaseapp.com",
+    projectId: "speedcubing-pb-tracker"
+}
---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "./firebase";
+initializeApp(firebaseConfig);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
