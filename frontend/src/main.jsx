3b0033a720e32316cc5fae187e333dddec564c23
Juwhan Jung <myorange02@Juwhanui-MacBookPro.local>
2025-08-28 14:35:02 +0900

    feat(frontend): React init

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 
+// Firebase init
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebase";
 initializeApp(firebaseConfig);
@@ -11,4 +11,4 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
